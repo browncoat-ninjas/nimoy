a3a8315792034abc58e4c9de238a841d5b1c4fc9
Yoav Luft <yoav.luft@ajillionmax.com>
2015-03-05 10:34:06 +0200

    Stubbing blocks

--- nimoy/feature.py
+++ nimoy/feature.py
@@ -1,4 +1,36 @@
 __author__ = 'luftzug'
+import inspect
+import ast
+
+
+class Then():
+    """
+    This manager actually transforms the code in it's 'with' block so that every line is executed separately and
+    evaluated into a boolean value. At the end of the block False values treated as failed asserts and fail the test
+    """
+    pass
+
+    def __enter__(self):
+        print('Starting then block')
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print('Leaving then block')
+
+
+class When():
+    pass
+
+
+class Expect():
+    pass
+
+
+class Setup():
+    pass
+
+
+class Where():
+    pass
 
 
 class Feature():
@@ -17,4 +49,5 @@ class Feature():
         feature_method.is_feature = True
         return feature_method
 
-
+    def transform_then_blocks(self):
+        pass
--- nimoy/tests.py
+++ nimoy/tests.py
@@ -1,5 +1,5 @@
 import unittest
-from nimoy.feature import Feature
+from nimoy.feature import Feature, Then
 from nimoy.spec import Specification
 
 __author__ = 'luftzug'
@@ -14,9 +14,25 @@ class SpecTesting(unittest.TestCase):
 
             @Feature('Feature test')
             def feature1(self):
-                pass
+                return True
 
         assert hasattr(Spec, 'features')
         assert not hasattr(Spec, 'feature1')
         assert 'Feature test' in Spec.features
         assert callable(Spec.features['Feature test'])
+        return_value = Spec.features['Feature test'](Spec())
+        assert return_value
+
+
+class ThenBlockTransforming():
+
+    def exploration_test(self):
+        outer_var = ''
+        then_manager = Then()
+        def method_with_then():
+            outer_var = 'outer'
+            with then_manager:
+                'out' in outer_var
+                True
+                1 > 2
+        method_with_then()
