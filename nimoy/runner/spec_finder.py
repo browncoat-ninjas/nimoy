a6a34ebf3cb5d524f4eb43d27f1e52cdc3e215bb
noamt <noam@10ne.org>
2017-07-30 18:07:11 +0300

    Piecing together a Spec runner - starting with the Spec finder component

---
+++ nimoy/runner/spec_finder.py
@@ -0,0 +1,37 @@
+import fnmatch
+import os
+
+
+class SpecFinder:
+    def __init__(self, working_directory) -> None:
+        super().__init__()
+        self.working_directory = working_directory
+        self.spec_locations = []
+
+    def find(self, suggested_locations):
+        if len(suggested_locations) == 0:
+            suggested_locations.append(self.working_directory)
+        self._find_specs_in_suggested_locations(suggested_locations)
+
+        return self.spec_locations
+
+    def _find_specs_in_suggested_locations(self, suggested_locations):
+
+        for suggested_location in suggested_locations:
+            normalized_suggested_location = self._normalize_suggested_location(suggested_location)
+
+            if normalized_suggested_location.endswith('spec.py'):
+                self.spec_locations.append(normalized_suggested_location)
+            else:
+                self._find_specs_in_directory(normalized_suggested_location)
+
+    def _normalize_suggested_location(self, suggested_location):
+
+        if os.path.isabs(suggested_location):
+            return suggested_location
+        return os.path.join(self.working_directory, suggested_location)
+
+    def _find_specs_in_directory(self, directory):
+        for root, dir_names, file_names in os.walk(directory):
+            for filename in fnmatch.filter(file_names, '*spec.py'):
+                self.spec_locations.append(os.path.join(root, filename))
---
+++ tests/nimoy/runner/test_spec_finder.py
@@ -0,0 +1,32 @@
+import tempfile
+import unittest
+import os
+from nimoy.runner.spec_finder import SpecFinder
+
+
+class TestSpecFinder(unittest.TestCase):
+    def setUp(self):
+        self.temp_spec = tempfile.NamedTemporaryFile(suffix='_spec.py')
+
+    def tearDown(self):
+        os.remove(self.temp_spec.name)
+
+    def test_implicit_location(self):
+        spec_locations = SpecFinder(os.path.dirname(self.temp_spec.name)).find([])
+        self.assertEquals(len(spec_locations), 1)
+        self.assertRegex(spec_locations[0], self.temp_spec.name)
+
+    def test_explicit_spec_path(self):
+        spec_locations = SpecFinder('/some/working/dir').find([self.temp_spec.name])
+        self.assertEquals(len(spec_locations), 1)
+        self.assertRegex(spec_locations[0], self.temp_spec.name)
+
+    def test_explicit_spec_directory(self):
+        spec_locations = SpecFinder('/some/working/dir').find([os.path.dirname(self.temp_spec.name)])
+        self.assertEquals(len(spec_locations), 1)
+        self.assertRegex(spec_locations[0], self.temp_spec.name)
+
+    def test_relative_spec_path(self):
+        spec_locations = SpecFinder('/some/working/dir').find(['jim_spec.py'])
+        self.assertEquals(len(spec_locations), 1)
+        self.assertRegex(spec_locations[0], '/some/working/dir/jim_spec.py')
