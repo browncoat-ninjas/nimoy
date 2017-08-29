29b4e482e43e827f66d5b72b131edb6feec98da3
noamt <noam@10ne.org>
2017-08-29 11:44:44 +0300

    Adding more helpful AST transformations: When finding a spec class , make sure it inherits from the unittest `TestCase` class

--- nimoy/ast_tools/spec_transformer.py
+++ nimoy/ast_tools/spec_transformer.py
@@ -1,4 +1,5 @@
 import ast
+import _ast
 from nimoy.ast_tools.ast_metadata import SpecMetadata
 
 
@@ -10,6 +11,11 @@ class SpecTransformer(ast.NodeTransformer):
     def visit_ClassDef(self, class_node):
         if class_node.name.endswith('Spec'):
             self._register_spec(class_node)
+            class_node.bases.append(_ast.Attribute(
+                value=_ast.Name(id='unittest', ctx=_ast.Load()),
+                attr='TestCase',
+                ctx=_ast.Load()
+            ))
         return class_node
 
     def _register_spec(self, class_node):
--- tests/nimoy/ast_tools/test_spec_transformer.py
+++ tests/nimoy/ast_tools/test_spec_transformer.py
@@ -4,7 +4,7 @@ from nimoy.ast_tools.spec_transformer import SpecTransformer
 
 
 class TestSpecTransformer(unittest.TestCase):
-    def test_find_spec(self):
+    def test_find_specs_in_module(self):
         spec_definition = 'class JimbobSpec:\n    pass\n\nclass JonesSpec:\n    pass\n\nclass Bobson:\n    pass\n'
         node = ast.parse(spec_definition, mode='exec')
 
@@ -14,3 +14,9 @@ class TestSpecTransformer(unittest.TestCase):
         self.assertEqual(len(found_metadata), 2)
         self.assertEqual(found_metadata[0].name, 'JimbobSpec')
         self.assertEqual(found_metadata[1].name, 'JonesSpec')
+
+        self.assertEqual(node.body[0].bases[0].attr, 'TestCase')
+        self.assertEqual(node.body[0].bases[0].value.id, 'unittest')
+        self.assertEqual(node.body[1].bases[0].attr, 'TestCase')
+        self.assertEqual(node.body[1].bases[0].value.id, 'unittest')
+        self.assertEqual(len(node.body[2].bases), 0)
