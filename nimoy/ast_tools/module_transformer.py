6b1ef1ff86c6c2321b906fa4e754d85ba29ea6cd
noamt <noam@10ne.org>
2017-08-30 11:31:46 +0300

    Piecing together a Spec runner: Also inject a nimoy module dependency, if it doesn't already exist

--- nimoy/ast_tools/module_transformer.py
+++ nimoy/ast_tools/module_transformer.py
@@ -5,13 +5,17 @@ import _ast
 class ModuleTransformer(ast.NodeTransformer):
     def visit_Module(self, module_node):
         import_statements = ModuleTransformer._gather_all_import_statements(module_node)
-        is_unittest_already_imported = ModuleTransformer._import_statements_include_unittest(import_statements)
 
-        if not is_unittest_already_imported:
-            module_node.body.insert(0, _ast.Import(
-                names=[_ast.alias(name='unittest')]
-            ))
+        required_modules = ['unittest', 'nimoy']
+        [ModuleTransformer._import_module_if_not_imported(module_node, import_statements, required_module) for
+         required_module in required_modules]
         return module_node
+
+    @staticmethod
+    def _import_module_if_not_imported(module_node, import_statements, required_module):
+        is_module_already_imported = ModuleTransformer._import_statements_include(import_statements, required_module)
+        if not is_module_already_imported:
+            ModuleTransformer._add_module_dependency(module_node, required_module)
 
     @staticmethod
     def _gather_all_import_statements(module_node):
@@ -20,10 +24,14 @@ class ModuleTransformer(ast.NodeTransformer):
         return import_statements
 
     @staticmethod
-    def _import_statements_include_unittest(import_statements):
-        return any(
-            ModuleTransformer._is_unittest_import_statement(import_statement) for import_statement in import_statements)
+    def _import_statements_include(import_statements, module_name):
+        return any(ModuleTransformer._is_module_import_statement(import_statement, module_name) for import_statement in
+                   import_statements)
 
     @staticmethod
-    def _is_unittest_import_statement(import_statement):
-        return any(import_name.name == 'unittest' for import_name in import_statement.names)
+    def _is_module_import_statement(import_statement, module_name):
+        return any(import_name.name == module_name for import_name in import_statement.names)
+
+    @staticmethod
+    def _add_module_dependency(module_node, module_name):
+        module_node.body.append(_ast.Import(names=[_ast.alias(name=module_name)]))
--- tests/nimoy/ast_tools/test_module_transformer.py
+++ tests/nimoy/ast_tools/test_module_transformer.py
@@ -10,19 +10,25 @@ class TestModuleTransformer(unittest.TestCase):
         node = ast.parse(module_definition, mode='exec')
 
         ModuleTransformer().visit(node)
-        self.assertEqual(len(node.body), 2)
-        self.assertTrue(isinstance(node.body[0], _ast.Import))
-        self.assertEqual(node.body[0].names[0].name, 'unittest')
+        self.assertEqual(len(node.body), 3)
+        self.assertTrue(isinstance(node.body[0], _ast.ClassDef))
 
-        self.assertTrue(isinstance(node.body[1], _ast.ClassDef))
+        self.assertTrue(isinstance(node.body[1], _ast.Import))
+        self.assertEqual(node.body[1].names[0].name, 'unittest')
+
+        self.assertTrue(isinstance(node.body[2], _ast.Import))
+        self.assertEqual(node.body[2].names[0].name, 'nimoy')
 
     def test_unittest_module_is_not_re_imported_if_already_exists(self):
-        module_definition = 'import unittest\n\nclass JimbobSpec:\n    pass\n\n'
+        module_definition = 'import unittest\nimport nimoy\n\nclass JimbobSpec:\n    pass\n\n'
         node = ast.parse(module_definition, mode='exec')
 
         ModuleTransformer().visit(node)
-        self.assertEqual(len(node.body), 2)
+        self.assertEqual(len(node.body), 3)
         self.assertTrue(isinstance(node.body[0], _ast.Import))
         self.assertEqual(node.body[0].names[0].name, 'unittest')
 
-        self.assertTrue(isinstance(node.body[1], _ast.ClassDef))
+        self.assertTrue(isinstance(node.body[1], _ast.Import))
+        self.assertEqual(node.body[1].names[0].name, 'nimoy')
+
+        self.assertTrue(isinstance(node.body[2], _ast.ClassDef))
