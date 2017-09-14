57c3e80df50ce3a5644fa05583ebb2c62bb08338
noamt <noam@10ne.org>
2017-09-14 10:53:48 +0300

    Enrich and refine the `where` block integration tests

--- tests/nimoy/integration/test_where_block.py
+++ tests/nimoy/integration/test_where_block.py
@@ -20,6 +20,27 @@ class JimbobSpec(Specification):
 
         result = self._run_spec_contents(spec_contents)
         self.assertTrue(result.wasSuccessful())
+        self.assertEqual(result.testsRun, 4)
+
+    def test_single_variable_with_single_value(self):
+        spec_contents = """from nimoy.specification import Specification
+        
+class JimbobSpec(Specification):
+    
+    def test(self):
+        with given:
+            a = value_of_a
+            
+        with expect:
+            (a % 2) == 0
+        
+        with where:
+            value_of_a = [2]
+        """
+
+        result = self._run_spec_contents(spec_contents)
+        self.assertTrue(result.wasSuccessful())
+        self.assertEqual(result.testsRun, 1)
 
     def test_single_variable_and_fail(self):
         spec_contents = """from nimoy.specification import Specification
@@ -39,6 +60,27 @@ class JimbobSpec(Specification):
 
         result = self._run_spec_contents(spec_contents)
         self.assertFalse(result.wasSuccessful())
+        self.assertEqual(result.testsRun, 2)
+
+    def test_single_variable_with_single_value_and_fail(self):
+        spec_contents = """from nimoy.specification import Specification
+        
+class JimbobSpec(Specification):
+    
+    def test(self):
+        with given:
+            a = value_of_a
+            
+        with expect:
+            (a % 2) == 0
+        
+        with where:
+            value_of_a = [3]
+        """
+
+        result = self._run_spec_contents(spec_contents)
+        self.assertFalse(result.wasSuccessful())
+        self.assertEqual(result.testsRun, 1)
 
     def test_list_form_multi_variables(self):
         spec_contents = """from nimoy.specification import Specification
@@ -61,6 +103,30 @@ class JimbobSpec(Specification):
 
         result = self._run_spec_contents(spec_contents)
         self.assertTrue(result.wasSuccessful())
+        self.assertEqual(result.testsRun, 3)
+
+    def test_list_form_multi_variables_with_a_single_value(self):
+        spec_contents = """from nimoy.specification import Specification
+        
+class JimbobSpec(Specification):
+    
+    def test(self):
+        with given:
+            a = value_of_a
+            b = value_of_b
+            
+        with expect:
+            (a * b) == expected_value
+        
+        with where:
+            value_of_a = [2]
+            value_of_b = [1]
+            expected_value = [2]
+        """
+
+        result = self._run_spec_contents(spec_contents)
+        self.assertTrue(result.wasSuccessful())
+        self.assertEqual(result.testsRun, 1)
 
     def test_list_form_multi_variables_and_fail(self):
         spec_contents = """from nimoy.specification import Specification
@@ -83,6 +149,30 @@ class JimbobSpec(Specification):
 
         result = self._run_spec_contents(spec_contents)
         self.assertFalse(result.wasSuccessful())
+        self.assertEqual(result.testsRun, 2)
+
+    def test_list_form_multi_variables_with_a_single_value_and_fail(self):
+        spec_contents = """from nimoy.specification import Specification
+        
+class JimbobSpec(Specification):
+    
+    def test(self):
+        with given:
+            a = value_of_a
+            b = value_of_b
+            
+        with expect:
+            (a * b) == expected_value
+        
+        with where:
+            value_of_a = [1]
+            value_of_b = [1]
+            expected_value = [5]
+        """
+
+        result = self._run_spec_contents(spec_contents)
+        self.assertFalse(result.wasSuccessful())
+        self.assertEqual(result.testsRun, 1)
 
     def test_matrix_form_multi_variables(self):
         spec_contents = """from nimoy.specification import Specification
@@ -106,6 +196,29 @@ class JimbobSpec(Specification):
 
         result = self._run_spec_contents(spec_contents)
         self.assertTrue(result.wasSuccessful())
+        self.assertEqual(result.testsRun, 3)
+
+    def test_matrix_form_multi_variables_with_a_single_value(self):
+        spec_contents = """from nimoy.specification import Specification
+        
+class JimbobSpec(Specification):
+    
+    def test(self):
+        with given:
+            a = value_of_a
+            b = value_of_b
+            
+        with expect:
+            (a * b) == expected_value
+        
+        with where:
+            value_of_a | value_of_b | expected_value
+            2          | 1          | 2
+        """
+
+        result = self._run_spec_contents(spec_contents)
+        self.assertTrue(result.wasSuccessful())
+        self.assertEqual(result.testsRun, 1)
 
     def test_matrix_form_multi_variables_and_fail(self):
         spec_contents = """from nimoy.specification import Specification
@@ -128,3 +241,26 @@ class JimbobSpec(Specification):
 
         result = self._run_spec_contents(spec_contents)
         self.assertFalse(result.wasSuccessful())
+        self.assertEqual(result.testsRun, 2)
+
+    def test_matrix_form_multi_variables_with_a_single_value_and_fail(self):
+        spec_contents = """from nimoy.specification import Specification
+        
+class JimbobSpec(Specification):
+    
+    def test(self):
+        with given:
+            a = value_of_a
+            b = value_of_b
+            
+        with expect:
+            (a * b) == expected_value
+        
+        with where:
+            value_of_a | value_of_b | expected_value
+            2          | 1          | 10
+        """
+
+        result = self._run_spec_contents(spec_contents)
+        self.assertFalse(result.wasSuccessful())
+        self.assertEqual(result.testsRun, 1)
