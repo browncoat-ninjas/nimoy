01a06623871fff2f0128af01599ea5382c35de33
noamt <noam@10ne.org>
2018-01-03 15:51:25 +0200

    Gradual dogfooding of our tests

---
+++ specs/nimoy/specification_spec.py
@@ -0,0 +1,85 @@
+from unittest import mock
+from nimoy.specification import Specification
+
+
+class FeatureBlockRuleEnforcerSpec(Specification):
+    def where_function_is_called_before_feature(self):
+        with expect:
+            class SomeSpec(Specification):
+                def test_something(self, where_visited=False):
+                    where_visited == True
+
+                def test_something_where(self, data_to_inject):
+                    isinstance(data_to_inject, dict) == True
+                    data_to_inject['where_visited'] = [True]
+
+            SomeSpec().test_something()
+
+    def normal_feature_is_called(self):
+        with expect:
+            class SomeSpec(Specification):
+                def test_something(self, where_visited=False):
+                    where_visited == False
+
+            SomeSpec().test_something()
+
+
+class InternalSpecificationMethodsSpec(Specification):
+    def feature_block_context_is_returned(self):
+        with given:
+            class SomeSpec(Specification):
+                pass
+
+            spec = SomeSpec()
+
+        with when:
+            feature_block = spec._feature_block_context('jimbob')
+
+        with then:
+            feature_block.block_type == 'jimbob'
+            feature_block.thrown_exceptions == spec.thrown_exceptions
+
+    @mock.patch('nimoy.specification.Compare')
+    def internal_comparison_is_called(self, compare_mock):
+        with given:
+            class SomeSpec(Specification):
+                pass
+
+        with when:
+            SomeSpec()._compare('a', 'b', 'some_name')
+
+        with then:
+            1 * compare_mock.return_value.compare('a', 'b', 'some_name')
+
+    @mock.patch('nimoy.specification.MockAssertions')
+    def internal_mock_assertion_is_performed(self, mock_assertions_mock):
+        with given:
+            mock_assertions_mock.return_value._mock_unsafe = True
+
+            class SomeSpec(Specification):
+                pass
+
+            some_mock = mock.Mock()
+            args = ['a', 'b']
+
+        with when:
+            SomeSpec()._assert_mock(1, some_mock, 'some_method', args)
+
+        with then:
+            1 * mock_assertions_mock.return_value.assert_mock(1, some_mock, 'some_method', args)
+
+    @mock.patch('nimoy.specification.ExceptionAssertions')
+    def internal_exception_assertion_is_performed(self, exception_assertions_mock):
+        with given:
+            exception_assertions_mock.return_value._mock_unsafe = True
+
+            class SomeSpec(Specification):
+                pass
+
+            spec = SomeSpec()
+
+        with when:
+            spec._exception_thrown(ArithmeticError)
+
+        with then:
+            1 * exception_assertions_mock.return_value.assert_exception(spec.thrown_exceptions, ArithmeticError)
