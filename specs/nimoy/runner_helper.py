c584a2d17381d5569f3d7b6693df75c27dd08961
noamt <noam@10ne.org>
2019-10-21 17:09:01 +0200

    #13: Check for `@unittest.skip` decorator before adding the feature for execution

--- specs/nimoy/runner_helper.py
+++ specs/nimoy/runner_helper.py
@@ -7,5 +7,4 @@ from nimoy.spec_runner import SpecRunner
 def run_spec_contents(spec_contents):
     str_io = StringIO()
     execution_framework = UnitTestExecutionFramework(stream=str_io)
-    result = SpecRunner._run_on_contents(execution_framework, [('/fake/path.py', spec_contents)])
-    return result
+    return SpecRunner._run_on_contents(execution_framework, [('/fake/path.py', spec_contents)])
