823de953d10d99ad02a7c8d8eaa66a2aa5fb08b0
noamt <noam@10ne.org>
2021-01-10 15:37:24 +0200

    Bump version to 1.0.2

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open('README.rst') as read_me:
 
 setup(
     name='nimoy-framework',
-    version='1.0.1',
+    version='1.0.2',
     description='A testing and specification framework for Python 3, heavily inspired by the Spock Framework',
     long_description=long_description,
     url='https://github.com/browncoat-ninjas/nimoy',
